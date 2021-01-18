cfdc16dd07b5641993c4d62a643e11318dcf2713
Vijay <vijay.gowda95@gmail.com>
2021-01-18 22:34:06 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,14 +1,5 @@
-public class Factorial {
-
- public static void main(String[] args) {
-
-  int number = 4;
-  int factorial = number;
-
-  for (int i = (number - 1); i & gt; 1; i--) {
-   factorial = factorial * i ;
-  }
-
-  System.out.println("Factorial of " + number + " is " + factorial);
- }
+class Factorial {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
